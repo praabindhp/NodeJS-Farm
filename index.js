7f2b213560e7af43fab8544f0478a40564639f4f
praabindh <praabindh@codingmart.com>
2023-02-13 17:10:26 +0530

    Worked On Praabindh's Farm Project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require( 'url');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 // const hello = 'Hello World!';
 // console.log(hello);
@@ -42,20 +43,6 @@ const url = require( 'url');
 // console.log('Will Read File !');
 
 // Creating The Server
-
-const replaceTemplates = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-};
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
