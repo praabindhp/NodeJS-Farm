ac9df5d2298376e17f8a1222096e9cae883175df
praabindh <praabindh@codingmart.com>
2023-02-13 15:23:38 +0530

    Worked On Praabindh's Farm Project

--- index.js
+++ index.js
@@ -43,6 +43,20 @@ const url = require( 'url');
 
 // Creating The Server
 
+const replaceTemplates = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+};
+
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -56,7 +70,11 @@ const server = http.createServer((req, res) => {
     // Overview Page
     if(pathName === '/' || pathName === '/overview') {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(tempOverview);
+
+        const cardsHTML = dataObj.map(el => replaceTemplates(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHTML);
+        console.log(cardsHTML);
+        res.end(output);
 
     // Product Page
     } else if(pathName === '/product') {
