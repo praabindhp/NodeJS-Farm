18e72a40582f5249abfe6e4ddcd78e7daefae1a9
praabindh <praabindh@codingmart.com>
2023-02-13 12:06:29 +0530

    Worked With NodeJS

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ console.log(hello);
 const textIn = fs.readFileSync('txt/input.txt', 'utf8');
 console.log(textIn);
 
-const textOut = `This is what we know about avacado : ${textIn}`;
+const textOut = `This is what we know about avacado : ${textIn}.\nCreated on ${Date.now()}`;
 console.log(textOut);
+fs.writeFileSync('txt/output.txt', textOut)
+console.log('File Write Success !');
