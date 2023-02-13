96877e9041eb50296f1f95cc5c763c5b1caaba51
praabindh <praabindh@codingmart.com>
2023-02-13 14:58:05 +0530

    Worked With NodeJS

--- index.js
+++ index.js
@@ -43,24 +43,33 @@ const url = require( 'url');
 
 // Creating The Server
 
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
+    // Overview Page
     if(pathName === '/' || pathName === '/overview') {
-        res.end('Welcome To Overview');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(tempOverview);
 
+    // Product Page
     } else if(pathName === '/product') {
         res.end('Welcome To Product');
 
+    // API Page
     } else if(pathName === '/api') {
             res.writeHead(200, {
                 'Content-Type': 'application/json'
             });
             res.end(data);
-
+    
+    // Page Not Found
     } else {
         res.writeHead(404, {
             'Content-Type': 'text/html',
