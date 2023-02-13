b27e3a728b0616392f0197a377156019ad2d5b75
praabindh <praabindh@codingmart.com>
2023-02-13 18:18:49 +0530

    Worked On Praabindh's Farm Project

--- index.js
+++ index.js
@@ -3,7 +3,18 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const slugify = require('slugify');
 const replaceTemplates = require('./modules/replaceTemplates');
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 
 // const hello = 'Hello World!';
 // console.log(hello);
@@ -43,13 +54,6 @@ const replaceTemplates = require('./modules/replaceTemplates');
 // console.log('Will Read File !');
 
 // Creating The Server
-
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
